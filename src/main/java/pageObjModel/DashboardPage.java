ec6b00dcbc26e0847cb73fe16ebd5cbabc202fa6
Paul-Ayomide <paulsure.ayomide@gmail.com>
2021-04-20 14:59:12 +0100

    This is a new commit to add HomePage

--- src/main/java/pageObjModel/DashboardPage.java
+++ src/main/java/pageObjModel/DashboardPage.java
@@ -1,9 +1,7 @@
 package pageObjModel;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 
 
---
+++ src/main/java/pageObjModel/HomePage.java
@@ -0,0 +1,5 @@
+package pageObjModel;
+
+public class HomePage {
+
+}
