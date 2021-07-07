bc325b907e111dc56c0ff25237f71b8965503cac
Paul Oluwagbemiga <paulsure.ayomide@gmail.com>
2021-07-07 09:55:44 +0100

    Update LoginPage.java
    rm vital info

--- src/main/java/pageObjModel/LoginPage.java
+++ src/main/java/pageObjModel/LoginPage.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 
-	// public static String PAGE_URL="https://iecr.softalliance.com/";
+	
 	 
   final WebDriver driver;
 	 
